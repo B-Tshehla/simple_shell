4a4f8bcd385565dbd25e1f42355961dc156094e8
B-Tshehla <Boitumelotshehla@gmail.com>
2022-06-09 13:15:44 +0200

    shell.c with potential bugs

--- shell.h
+++ shell.h
@@ -1,12 +1,15 @@
 #ifndef SHELL_H
 #define SHELL_H
 
-#include <stdio.h>
+#include <fcntl.h>
+#include <signal.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/wait.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <sys/types.h>
-#include <sys/wait.h>
-#include <string.h>
+#include <errno.h>
+#include <stdio.h>
 
 int _putchar(char c);
 
@@ -19,5 +22,10 @@ char *_strchr(char *s, char c);
 int _strspn(char *s, char *accept);
 int _strcmp(char *s1, char *s2);
 int _strncmp(const char *s1, const char *s2, size_t n);
+
+/* shell.c prototypes*/
+void sig_handler(int sig);
+int execute(char **args, char **front);
+
 
 #endif
